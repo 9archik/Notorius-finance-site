f512d6a8dd418d42c9ac1df8979affe5412b46fb
9archik <109422607+9archik@users.noreply.github.com>
2022-09-12 00:15:08 +0300

    script1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('active') 
     burgerMenu.classList.toggle('active')
     body.classList.toggle('active');
-=======
+}
 let burgerBtn = document.querySelector('.burger__btn');
 
 let burgerMenu = document.querySelector('.burger__menu');
