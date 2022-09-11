9620ace302fed6e7b0b897b2c104fc959fd307e7
9archik <109422607+9archik@users.noreply.github.com>
2022-09-12 00:24:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let burgerBtn = document.querySelector('.burger__btn');
 
 let burgerMenu = document.querySelector('.burger__menu');
@@ -9,16 +8,4 @@ burgerBtn.addEventListener('click', () => {
     burgerBtn.classList.toggle('active') 
     burgerMenu.classList.toggle('active')
     body.classList.toggle('active');
-})
-let burgerBtn = document.querySelector('.burger__btn');
-
-let burgerMenu = document.querySelector('.burger__menu');
-
-let body=document.getElementsByTagName('body')[0]
-
-burgerBtn.addEventListener('click', () => { 
-    burgerBtn.classList.toggle('active') 
-    burgerMenu.classList.toggle('active')
-    body.classList.toggle('active');
-
 })
